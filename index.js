59d3e5399e6e57e76ccb814ca9f133472d2f5898
Adam Sheeres-Paulicpulle <asheeres@uoguelph.ca>
2021-10-27 13:51:24 -0400

    Successfully scraped webpage

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
     By: Adam Sheeres-Paulicpulle
 */
 
+const PORT = 8000;
+
 //packages installed
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -10,3 +12,27 @@ const express = require('express');
 
 //init express
 const app = express();
+
+const url = 'https://www.theguardian.com/international';
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+
+        const articles = []
+
+
+        $('.fc-item__title ', html).each(function() {
+            const title = $(this).text()
+            const lookingURL = $(this).find('a').attr('href')
+            articles.push({
+                title,
+                lookingURL
+            })
+        })
+        console.log(articles)
+
+    }).catch(err => console.log(err))
+
+app.listen(PORT, () => console.log(`sever running on PORT ${PORT}`));
+
